d0719a869d86747c134616e0f2c846ae14cd063a
flines <flines@126.com>
2019-12-21 13:15:17 +0800

    Create index.jsx

---
+++ packages/ScrollLayout/index.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useContext } from 'react';
+import PropTypes from 'prop-types';
+
+export const ScrollLayout = (props) => {
+    
+    return (
+        <div></div>
+    );
+}; 
