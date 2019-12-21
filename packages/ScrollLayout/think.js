b420571c5da19c14739d4c0c9279b690e5ddf34b
flines <flines@126.com>
2019-12-21 14:04:19 +0800

    Update think.js
    think scrollsync

--- packages/ScrollLayout/think.js
+++ packages/ScrollLayout/think.js
@@ -5,15 +5,96 @@
  maxso: containerHeight - viewPortHeight, (0 < maxso)
  ivpo: innerHeight - viewPortHeight, (any)
  so: scrollOffset, (0<= so <= maxso)
- 
+ isScrollDown
 */
 
 export const ScrollSync = () => {
+    let vph, ch ,ih ,maxso, ivpo, so;
+    let isScrollDown;
+    let prer;
+    
+    reset();
+    resetPrer();
+    
     return {
-        
+        run
     };
     
+    function reset() {
+        vph = null;
+        ch = null;
+        ih = null;
+        maxso = null;
+        ivpo = null;
+        so = null;
+        isScrollDown = null;
+    }
+    
+    function resetPrer() {
+        prer = {
+            vph: null,
+            ch: null,
+            ih: null,
+            maxso: null,
+            ivpo: null,
+            so: null,
+            isScrollDown: null
+        };
+    }
+    
+    function updatePrer() {
+        prer = {
+            vph,
+            ch,
+            ih,
+            maxso,
+            ivpo,
+            so,
+            isScrollDown
+        };
+    }
+    
+    function calc(params) {
+        
+    }
+    
+    function checkIsScrollDown(soIn, preSoIn, preIsScrollDownIn) {
+        if (isNil(preSoIn) || isNil(so)) return null;
+        if (soIn === preSoIn) return preIsScrollDownIn;
+        if (soIn > preSoIn) return true;
+        if (soIn < preSoIn) return false;
+        return null;
+    }
+    
     function run(params) {
-        const { scrollTop,  }
+        const { scrollOffset, containerHeight, innerHeight, viewPortHeight } = params || {};
+        so = scrollOffset;
+        ch = containerHeight;
+        ih = innerHeight;
+        vph = viewPortHeight;
+        isScrollDown = checkIsScrollDown(so, prer.so, prer.isScrollDown);
+        onScroll();
+        updatePrer();
+    }
+    
+    function onScroll() {
+        if (isNil(isScrollDown)) return;
+        if (isScrollDown) {
+            scrollDown();
+        } else {
+            scrollUp();
+        }
+    }
+    
+    scrollDown() {
+    
+    }
+    
+    scrollUp() {
+        
+    }
+        
+    function isNil(o) {
+        return o === null || o === undefined;
     }
 };
