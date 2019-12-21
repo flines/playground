12c5c729cf9247c13c0a6be3dafaacef44269909
flines <flines@126.com>
2019-12-21 13:32:50 +0800

    Create think.js

---
+++ packages/ScrollLayout/think.js
@@ -0,0 +1,19 @@
+/* snap: which has the scrollbar
+ vph: viewPortHeight, (0 <= vph)
+ ch: containerHeight, (0 <= ih <= ch)
+ ih: innerHeight, (0 <= ih)
+ maxso: containerHeight - viewPortHeight, (0 < maxso)
+ ivpo: innerHeight - viewPortHeight, (any)
+ so: scrollOffset, (0<= so <= maxso)
+ 
+*/
+
+export const ScrollSync = () => {
+    return {
+        
+    };
+    
+    function run(params) {
+        const { scrollTop,  }
+    }
+};
